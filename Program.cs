8cbb2d67dc67ef31efb718c3c73d889181b72b67
VenturingForth <joshua@codeadventure.uk>
2024-06-24 13:53:40 +0100

    Added a Dice class with Roll method.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var random = new Random();
+var dice = new Dice(random);
+Console.WriteLine(dice.Roll());
+
+Console.ReadKey();
+
+public class Dice()
+{
+    private readonly Random _random;
+    private const int SidesCount = 6;
+
+    public Dice(Random random) : this()
+    {
+        this._random = random;
+    }
+
+    public int Roll()
+    {
+        return _random.Next(1, SidesCount + 1);
+    }
+}
