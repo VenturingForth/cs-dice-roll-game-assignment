fae04358177394ad2fde1785f4cc11fca3059ade
VenturingForth <joshua@codeadventure.uk>
2024-06-24 14:40:39 +0100

    Implemented victory and loss conditions as enum. Added classes for play through and guessing.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,59 @@
 ﻿var random = new Random();
 var dice = new Dice(random);
-Console.WriteLine(dice.Roll());
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
 
 Console.ReadKey();
+
+public class GuessingGame
+{
+    private readonly Dice _dice;
+    private const int NumberOfTries = 3;
+
+    public GuessingGame(Dice dice)
+    {
+        _dice = dice;
+    }
+
+    public GameResult Play()
+    {
+        int diceRollResult = _dice.Roll();
+        Console.WriteLine($"Dice rolled. Guess what number it shows in {NumberOfTries} tries.");
+
+        int triesLeft = NumberOfTries;
+        while(triesLeft > 0)
+        {
+            int guess = ConsoleInterface.ReadInteger("Enter a number:");
+            if (guess == diceRollResult)
+            {
+                return GameResult.Victory
+            }
+            Console.WriteLine("Wrong number.");
+            --triesLeft;
+        }
+        return GameResult.Loss;
+    }
+}
+
+public enum GameResult
+{
+    Victory,
+    Loss
+}
+
+public static class ConsoleInterface
+{
+    public static int ReadInteger(string message)
+    {
+        do
+        {
+            Console.WriteLine(message);
+        }
+        while (!int.TryParse(Console.ReadLine(), out int result));
+        return result;
+    }
+}
 
 public class Dice()
 {
@@ -14,8 +65,5 @@ public class Dice()
         this._random = random;
     }
 
-    public int Roll()
-    {
-        return _random.Next(1, SidesCount + 1);
-    }
+    public int Roll() => _random.Next(1, SidesCount + 1);
 }
